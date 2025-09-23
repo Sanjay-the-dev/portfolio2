9c63631eacb57aa5352a7c1aa0e6511d8f876ebc
sanjay-the-dev <sanjay525522@gmail.com>
2025-09-23 19:36:54 +0530

    nav bar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {Link} from 'react-scroll'
+import menu_close from '../../assets/main assets/menu_close.svg'
+import menu_open from '../../assets/main assets/menu_open.svg'
 import './NavBar.css'
 
 const NavBar = () => {
+
+  const menuRef = useRef();
+
+  const closeMenu = ()=>{ menuRef.current.style.right="-450px"; console.log("workingi")}
+  const openMenu =()=>{menuRef.current.style.right = "0px"}
   return (
+    
     <div className='NavBar'>
-       <ul className='NavMenu'>
+
+      <img className='nav-mob-open' onClick={openMenu} src={menu_open} alt="" />
+
+       <ul className='NavMenu' ref={menuRef} >
+
+        <img className='nav-mob-close' src={menu_close} onClick={closeMenu} alt="" />
         <li> <Link className='anchor-link' offset={-250} smooth={true} to='Home'>Home</Link> </li>
         <li><Link className='anchor-link' offset={-50} smooth={true} to='About'>About</Link></li>
         <li><Link className='anchor-link' offset={-20} smooth={true} to='Projects'>Project</Link></li>
